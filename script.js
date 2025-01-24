82a4536e9db3f644bb5333d60b91734f9c3733c5
Pekko <Pekko.pulliainen@outlook.com>
2025-01-25 01:32:48 +0200

    commit

--- script.js
+++ script.js
@@ -18,6 +18,17 @@ let newRoll1;
 let newRoll2;
 let roundPoints = 0;
 let voittaja;
+let baseMusic;
+let totalPointsText = document.getElementById("totalPoints")
+
+const musicTracks = [
+  "audio/music1.mp3",
+  "audio/music2.mp3",
+];
+
+baseMusic = document.querySelector(".baseMusic");
+
+selectRandomMusic();
 
 const players = [];
 
@@ -25,9 +36,16 @@ if (dice_amount === 0) {
   questionText.textContent = "Monta noppaa?";
 }
 
+function selectRandomMusic() {
+  const randomIndex = Math.floor(Math.random() * musicTracks.length);
+  const selectedTrack = musicTracks[randomIndex];
+  baseMusic.src = selectedTrack;
+  console.log(selectedTrack);
+}
+
+
 function dieselect() {
   dice_amount = 1;
-  console.log(dice_amount + " noppa");
   dicechoice1.style.display = "none";
   dicechoice2.style.display = "none";
   questionText.textContent = "Syötä pelaajamäärä ja nimet.";
@@ -37,7 +55,6 @@ function dieselect() {
 
 function dice2select() {
   dice_amount = 2;
-  console.log("noppia " + dice_amount);
   dicechoice1.style.display = "none";
   dicechoice2.style.display = "none";
   questionText.textContent = "Syötä pelaajamäärä ja nimet.";
@@ -77,6 +94,7 @@ function startGame() {
   nameInputBox.style.display = "none";
   addPlayerButton.style.display = "none";
   questionText.textContent = players[currentTurn].nimi + " heitä noppaa!";
+  totalPointsText.textContent = "Pelaajan " + players[currentTurn].nimi + " pisteet: " + players[currentTurn].pisteet
   diceButton.style.display = "block";
   diceButton.textContent = "Heitä";
   startGameButton.textContent = "seuraava vuoro";
@@ -103,8 +121,9 @@ function rollDice() {
       diceRoll1 +
       "! Pisteet: " +
       roundPoints;
+      totalPointsText.textContent = "Pelaajan " + players[currentTurn].nimi + " oikeat pisteet: " + players[currentTurn].pisteet + " + " + roundPoints;
+    totalPointsText.style.display = "block";
     dontThrowButton.style.display = "block";
-    console.log("pisteet " + roundPoints);
   } else {
     diceRoll2 = Math.floor(Math.random() * 6) + 1;
     if (diceRoll1 === 1 || diceRoll2 === 1) {
@@ -136,8 +155,9 @@ function rollDice() {
       diceRoll2 +
       "! Pisteet: " +
       roundPoints;
+    totalPointsText.textContent = "Pelaajan " + players[currentTurn].nimi + " oikeat pisteet: " + players[currentTurn].pisteet + " + " + roundPoints;
+    totalPointsText.style.display = "block";  
     dontThrowButton.style.display = "block";
-    console.log("pisteet " + roundPoints);
   }
 }
 
@@ -153,11 +173,14 @@ function dontRoll() {
   dontThrowButton.style.display = "none";
   questionText.textContent =
     players[currentTurn].nimi + " pisteesi: " + players[currentTurn].pisteet;
+    totalPointsText.textContent = "Pelaajan " + players[currentTurn].nimi + " pisteet: " + players[currentTurn].pisteet
   currentTurn = (currentTurn + 1) % players.length;
   roundPoints = 0;
 }
 
 function endGame() {
+  const winsound = new Audio("audio/five-nights-at-freddys-6-am.mp3")
+  winsound.play()
   document.body.innerHTML = "";
 
   const victoryText = document.createElement("voittoText");
